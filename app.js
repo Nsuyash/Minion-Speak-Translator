bdb1bcb737229ae3d91f4bdbe35d583ea92e9f04
Suyash Nandurkar <suyashnandurkar53@gmail.com>
2023-10-13 01:53:22 +0530

    I/O Processing Done

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello!!");
+// var username = prompt("Give me you name");
+// alert("Welcome! "+username);
+
+var btnTranslate = document.querySelector("#btn-translate") // Here we query has been selected refrence to it id="btn-translate"
+//console.log((btnTranslate).style.backgroundColor = "red")
+
+var txtInput = document.querySelector("#txt-input");// Here I have selected the textarea using id="txt-input"
+
+var txtOutput = document.querySelector("#txt-Output");// Here I selected ouput box which I have created
+
+
+
+
+//console.log(txtInput.value)
+btnTranslate.addEventListener("click", function clickEventListener(){
+    //console.log("Clicked!")
+    //var show = txtInput.value;
+    //txtOutput.innerText = show;
+    
+})// This is an event listener on the action of "click"
