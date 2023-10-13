940a008c2eae6fa838f0846381ea3a1710b909db
Suyash Nandurkar <suyashnandurkar53@gmail.com>
2023-10-14 03:32:27 +0530

    Done

--- app.js
+++ app.js
@@ -4,17 +4,37 @@
 var btnTranslate = document.querySelector("#btn-translate") // Here we query has been selected refrence to it id="btn-translate"
 //console.log((btnTranslate).style.backgroundColor = "red")
 
-var txtInput = document.querySelector("#txt-input");// Here I have selected the textarea using id="txt-input"
+var txtInput = document.querySelector("#txt-input"); // Here I have selected the textarea using id="txt-input"
 
-var txtOutput = document.querySelector("#txt-Output");// Here I selected ouput box which I have created
-
-
+var txtOutput = document.querySelector("#txt-Output"); // Here I selected ouput box which I have created
 
 
-//console.log(txtInput.value)
-btnTranslate.addEventListener("click", function clickEventListener(){
+var serverURL = "https://api.funtranslations.com/translate/minion.json"
+
+
+//write a function to get input in URL .json?text="I am suyash" text
+function getTranslationURL(text) {
+    return serverURL + "?" + "text=" + text;
+}
+
+
+//write a function to fetch
+function clickEventListener() {
     //console.log("Clicked!")
     //var show = txtInput.value;
     //txtOutput.innerText = show;
-    
-})// This is an event listener on the action of "click"
+    var inputtext = txtInput.value;
+    //calling server for processing
+    fetch(getTranslationURL(inputtext))
+        .then(response => response.json())
+        .then(json => {
+            var translatedText = json.contents.translated;
+            txtOutput.innerText = translatedText;
+        })
+        .catch(error => alert("server is down"))
+
+}
+
+
+//console.log(txtInput.value)
+btnTranslate.addEventListener("click", clickEventListener) // This is an event listener on the action of "click"
